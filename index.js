177541a8d8e99f7fdcf533eb31967d15741d7d90
André <andre.katsuragawa@gmail.com>
2021-06-10 13:22:56 -0300

    Add basic calculator functions

--- index.js
+++ index.js
@@ -1,37 +1,83 @@
-"use strict";
-
-// get key elements
-const zero = document.getElementById("0");
-const one = document.getElementById("1");
-const two = document.getElementById("2");
-const three = document.getElementById("3");
-const four = document.getElementById("4");
-const five = document.getElementById("5");
-const six = document.getElementById("6");
-const seven = document.getElementById("7");
-const eight = document.getElementById("8");
-const nine = document.getElementById("9");
-const dot = document.getElementById("dot");
-const plus = document.getElementById("plus");
-const minus = document.getElementById("minus");
-const multiply = document.getElementById("multiply");
-const divide = document.getElementById("divide");
+// get elements
+const display = document.querySelector(".display-content");
+const numberKeys = document.querySelectorAll(".number");
+const operatorKeys = document.querySelectorAll(".operator");
 const del = document.getElementById("del");
-
-
+const reset = document.getElementById("reset");
 const equal = document.getElementById("equal");
-console.log(equal);
 
-// display
-const display = document.getElementsByClassName("display-content");
-display.innerHTML = "";
+let firstNum = 0;
+let secondNum = 0;
+let result = 0;
+let operator = "";
 
-console.log(display);
+// setup event handlers
+const handleNumberClick = (e) => {
+  console.log(`clicked ${e.target.innerText}`);
+  if (display.innerText[0] === "0") {
+    display.innerText = "";
+  }
+  const num = e.target.innerText;
+  if (display.innerText.length < 15) {
+    display.innerText += num;
+  } else {
+    alert("Number too long for display");
+  }
+};
 
+const handleOperation = (e) => {
+  operation = e.target.id;
+  if (display.innerText[0] !== "0") {
+    firstNum = Number(display.innerText);
+    console.log(`first number = ${firstNum}`);
+    display.innerText = "0";
+  }
+};
 
-console.log(two);
+const handleDel = (e) => {
+  display.innerText = display.innerText.slice(0, -1);
+  if (display.innerText.length === 0) {
+    display.innerText = "0";
+  }
+};
 
-one.addEventListener("click", () => {
-  console.log("clicked one");
-  display.innerText += 1;
-});
+const handleResult = (e) => {
+  secondNum = Number(display.innerText);
+  console.log(firstNum, operation, secondNum);
+  switch (operation) {
+    case "plus":
+      result = firstNum + secondNum;
+      break;
+    case "minus":
+      result = firstNum - secondNum;
+      break;
+    case "multiply":
+      result = firstNum * secondNum;
+      break;
+    case "divide":
+      result = firstNum / secondNum;
+      break;
+  }
+  if (String(result).length > 16) {
+    alert("Number too long for display");
+  } else {
+    display.innerText = result;
+  }
+};
+
+const handleReset = (e) => {
+  firstNum = 0;
+  secondNum = 0;
+  display.innerText = "0";
+};
+
+// add event listeners
+numberKeys.forEach((number) =>
+  number.addEventListener("click", handleNumberClick)
+);
+operatorKeys.forEach((operator) =>
+  operator.addEventListener("click", handleOperation)
+);
+del.addEventListener("click", handleDel);
+equal.addEventListener("click", handleResult);
+reset.addEventListener("click", handleReset);
